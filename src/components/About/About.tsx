76c2529dce0a574db7c61c113efb92977e5a8b56
Michael Akinyemi <makinyemi95@gmail.com>
2025-11-16 09:04:13 -0500

    Added about component page

---
+++ src/components/About/About.tsx
@@ -0,0 +1,16 @@
+import './About.css'
+
+const About = () => {
+  return (
+    <div className="Section">
+      <p>Welcome to my About Page</p>
+      <p>
+        Hi, Im a full stack software engineer who loves building resilient
+        backend systems and developing modern, responsive front-end
+        applications.{' '}
+      </p>
+    </div>
+  )
+}
+
+export default About
