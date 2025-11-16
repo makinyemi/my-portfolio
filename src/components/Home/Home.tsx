790e7bd8b78bcedd37fac4406dfcd5fed59cf160
Michael Akinyemi <makinyemi95@gmail.com>
2025-11-16 09:03:25 -0500

    Added home component page

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="Section">
+      <p>Welcome to my Homepage</p>
+      <p>second p</p>
+    </div>
+  )
+}
+
+export default Home
