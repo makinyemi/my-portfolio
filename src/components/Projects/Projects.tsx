9d0b32b308d341c47a4ee7a7479fa8576982010b
Michael Akinyemi <makinyemi95@gmail.com>
2025-11-16 15:48:13 -0500

    Project interface and state added

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react'
 import './Projects.css'
 
+interface Project {
+  id: number
+  title: string
+  thumbmail: string
+  githubLink: string
+  description: string
+}
 const Projects = () => {
+  const [projects, setProjects] = useState<Project[]>([])
+
   return (
     <div className="Section">
       <p>Welcome to my Projects Page</p>
