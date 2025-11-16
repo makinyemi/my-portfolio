68dc56eec0de082d78ec68e424d5e432a3a0a18b
Michael Akinyemi <makinyemi95@gmail.com>
2025-11-16 09:04:49 -0500

    Added projects component page

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,11 @@
+import './Projects.css'
+
+const Projects = () => {
+  return (
+    <div className="Section">
+      <p>Welcome to my Projects Page</p>
+    </div>
+  )
+}
+
+export default Projects
